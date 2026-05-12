70950aec62a7ec4aa182478152f721b977465a90
Astroth <denys.goral@gmail.com>
2026-05-12 15:04:14 +0300

    fix: fixed all icons and added them to UI

--- AstroLauncher/ViewModels/DiscoverViewModel.cs
+++ AstroLauncher/ViewModels/DiscoverViewModel.cs
@@ -11,6 +11,6 @@ public partial class DiscoverViewModel : ViewModelBase
     public DiscoverViewModel()
     {
         DisplayName = "Discover";
-        IconData = StreamGeometry.Parse("m16.24 7.76-1.804 5.411a2 2 0 0 1-1.265 1.265L7.76 16.24l1.804-5.411a2 2 0 0 1 1.265-1.265z"); 
+        IconData = (StreamGeometry)Application.Current.FindResource("DiscoverIcon"); 
     }
 }
--- AstroLauncher/ViewModels/SettingsPageViewModels/LauncherViewModel.cs
+++ AstroLauncher/ViewModels/SettingsPageViewModels/LauncherViewModel.cs
@@ -11,6 +11,6 @@ public partial class LauncherViewModel : ViewModelBase
     public LauncherViewModel()
     {
         DisplayName = "Launcher";
-        IconData = StreamGeometry.Parse("m14.305 7.53.923-.382 m15.228 4.852-.923-.383 m16.852 3.228-.383-.924 m16.852 8.772-.383.923 m19.148 3.228.383-.924 m19.53 9.696-.382-.924 m20.772 4.852.924-.383 m20.772 7.148.924.383 M22 13v2a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h7 M8 21h8");
+        IconData = (StreamGeometry)Application.Current.FindResource("LauncherIcon");
     }
 }
